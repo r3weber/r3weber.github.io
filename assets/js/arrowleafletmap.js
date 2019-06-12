21f67c3a460baa643246918b592f24e5eaff8388
r3weber <rweber@osmre.gov>
2019-06-12 14:35:35 -0600

    T

--- assets/js/arrowleafletmap.js
+++ assets/js/arrowleafletmap.js
@@ -1,6 +1,6 @@
 var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accesstoken}', {
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
         '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +

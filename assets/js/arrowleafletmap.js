63d57652e6a4fe0354f13a0dcd2c14c92ab9d31d
r3weber <rweber@osmre.gov>
2019-06-13 08:55:36 -0600

    map on load

--- assets/js/arrowleafletmap.js
+++ assets/js/arrowleafletmap.js
@@ -1,3 +1,7 @@
+
+function receivedNewEOSPosition(lat, lon, elv, numSatsUsed, pdop, hdop, vdop, diffAge, diffType, diffStn,
+xyzAccuracy, zAccuracy, xyAccuracy, geoidSep){
+
 var mymap = L.map('mapid').setView([39.7392, -104.9847], 13);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
@@ -8,15 +12,7 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     id: 'mapbox.streets',
     accessToken: 'pk.eyJ1IjoicjN3ZWJlcjEiLCJhIjoiY2lyM3lhc3FnMDFrZ2Zwbm04cncwa2JkMiJ9.AeYZqyDiobmuxAVfIKE8gA'
 }).addTo(mymap);
-
-
-
-// L.marker([51.5, -0.09]).addTo(mymap)
-//     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-//     .openPopup();
 mymap.on('load', function() {
-function receivedNewEOSPosition(lat, lon, elv, numSatsUsed, pdop, hdop, vdop, diffAge, diffType, diffStn,
-xyzAccuracy, zAccuracy, xyAccuracy, geoidSep){
 //  var geojsonFeature = {
 //     "type": "Feature",
 //     "properties": {
@@ -36,5 +32,6 @@ L.marker(latlng).addTo(mymap);
     // .bindPopup(`<h2>Lat: ${lat}</h2>\n<h2>Long:${lon}</h2>`)
     // .openPopup();
 L.map('mapid').setView(latlng, 10);  
-}
 });
+}
+

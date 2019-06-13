432409ee0abc6ac6b5b3a0241c6518fe3a75091f
r3weber <rweber@osmre.gov>
2019-06-13 13:01:06 -0600

    test

--- assets/js/arrowleafletmap.js
+++ assets/js/arrowleafletmap.js
@@ -1,4 +1,4 @@
-var mymap = L.map('mapid').setView([39.97668989, -105.26395322]);
+var mymap = L.map('mapid').setView([39.97668989, -105.26395322],13);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     maxZoom: 18,

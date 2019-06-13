f85782ca124076b054adf5bafa0b11460d53f1a1
r3weber <rweber@osmre.gov>
2019-06-13 09:13:10 -0600

    test

--- assets/js/arrowleafletmap.js
+++ assets/js/arrowleafletmap.js
@@ -1,8 +1,4 @@
-
-function receivedNewEOSPosition(lat, lon, elv, numSatsUsed, pdop, hdop, vdop, diffAge, diffType, diffStn,
-xyzAccuracy, zAccuracy, xyAccuracy, geoidSep){
-
-var mymap = L.map('mapid').setView([39.7392, -104.9847], 13);
+var mymap = L.map('mapid').setView([lat, lon], 13);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     maxZoom: 18,
@@ -13,25 +9,22 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     accessToken: 'pk.eyJ1IjoicjN3ZWJlcjEiLCJhIjoiY2lyM3lhc3FnMDFrZ2Zwbm04cncwa2JkMiJ9.AeYZqyDiobmuxAVfIKE8gA'
 }).addTo(mymap);
 mymap.on('load', function() {
-//  var geojsonFeature = {
-//     "type": "Feature",
-//     "properties": {
-//         "name": "Coors Field",
-//         "amenity": "Baseball Stadium",
-//         "numSatsUsed": numSatsUsed
-//     },
-//     "geometry": {
-//         "type": "Point",
-//         "coordinates": [lat, lon, elv]
-//     }
-// };
-// L.geoJSON(geojsonFeature).addTo(mymap);
-var latlng = L.latlon(lat, lon);
-console.log(latlng);
-L.marker(latlng).addTo(mymap);
+var position = []
+
+// var latlng = L.latlon(lat, lon);
+console.log(position);
+L.marker(position).addTo(mymap);
     // .bindPopup(`<h2>Lat: ${lat}</h2>\n<h2>Long:${lon}</h2>`)
     // .openPopup();
-L.map('mapid').setView(latlng, 10);  
-});
+L.map('mapid').setView(position, 10);  
+
+function receivedNewEOSPosition(lat, lon, elv, numSatsUsed, pdop, hdop, vdop, diffAge, diffType, diffStn,
+xyzAccuracy, zAccuracy, xyAccuracy, geoidSep){
+position.push(lat, lon)
+
+
+
+
 }
 
+});
